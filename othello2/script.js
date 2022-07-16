37e0c520ebaae0057f6096b19323fc6b2cef0912
Anii <79158719+lelouch-of-the-code@users.noreply.github.com>
2022-07-16 10:47:38 +0530

    minmax for othello

--- othello2/script.js
+++ othello2/script.js
@@ -1,18 +1,19 @@
 const dr = [1,1,1,-1,-1,-1,0,0];
 const dc = [1,-1,0,1,-1,0,1,-1];
 const inf = Math.floor(1e9);
+// const assert=require("assert");
 
+let isBot=1, botTurn=-1;
 
 function start(board) {
-    let sa=2,sb=2,turn=1,n=board.length;
-    updateScores(sa,sb);
+    let turn=1,n=board.length,disabled=0;
     display(board,turn);
     // add click listeners to all cells
     for(let i=0;i<n;i++) {
         for(let j=0;j<n;j++) {
             let cell=id(i,j,n);
             cell.addEventListener("click", function clickEvent() {
-                if(board[i][j]!=0) 
+                if(disabled||board[i][j]!=0) 
                     return;
                 let changed = makeMove(board,[i,j],turn);
                 if(!changed.length)
@@ -23,7 +24,21 @@ function start(board) {
                 board[i][j]=turn;
                 turn*=-1;
                 display(board,turn);
-                // console.log(turn);
+
+                disabled=isBot;
+                let bb=copyBoard(board);
+                let bM=minMax(bb,turn,6);
+                console.log("best move is: ", bM);
+                if(turn == botTurn && bM[1]!=-1)
+                    id(bM[1],bM[2],n).classList.add("bestMove");
+                setTimeout(function(){
+                    disabled=0;
+                    id(bM[1],bM[2],n).classList.remove("bestMove");
+                    if(isBot && turn==botTurn && bM[1]!=-1) {
+                        id(bM[1],bM[2],n).click();
+                    } 
+                },2000);
+                console.log("evalstatic is: ",evalStatic(board));
             });
         }
     }
@@ -57,6 +72,8 @@ function validMoves(board, turn) {
             }
         }
     }
+    if(!res.length)
+        return res;
     res.sort();
     uniq=[[res[0]%n, Math.floor(res[0]/n)]];
     for(let i=1;i<res.length;i++) {
@@ -69,8 +86,9 @@ function validMoves(board, turn) {
 
 function makeMove(board, move, turn) {
     // returns a list of squares that would be changed to turn
-    // by this move
+    // by this move excluding clicked sqaure
     // console.log(move);
+    console.assert(board[move[0]][move[1]]==0, "uwuw whyy");
     let n=board.length, r=move[0], c=move[1];
     let res=[];
     for(let d=0;d<8;d++) {
@@ -81,7 +99,7 @@ function makeMove(board, move, turn) {
         }
         let cur=[];
         while(true) {
-            if(!bound(rr,cc,n) || board[rr][cc]==turn) {
+            if(!bound(rr,cc,n) || board[rr][cc]!=turn*-1) {
                 break;
             } else {
                 cur.push([rr,cc]);
@@ -94,7 +112,7 @@ function makeMove(board, move, turn) {
             }
         }
     }
-    console.log(res);
+    // console.log(res);
     return res;
 }
 
@@ -125,12 +143,14 @@ function generateBoard(n) {
     return board;
 }
 
-// board[i][j]: 1:player0 -1:player1
 function display(board, turn) {
-    console.log(turn);
+    // console.log(turn);
+    let sc_a=0,sc_b=0;
     let n=board.length;
     for(let i=0;i<n;i++) {
         for(let j=0;j<n;j++) {
+            sc_a+=board[i][j]>0;
+            sc_b+=board[i][j]<0;
             let cell=id(i,j,n);
             cell.classList.remove("candidate");
             if(!board[i][j])
@@ -139,8 +159,8 @@ function display(board, turn) {
             cell.classList.add("p"+(board[i][j]));
         }
     }
+    updateScores(sc_a,sc_b,turn);
     let moves=validMoves(board, turn);
-    console.log(moves);
     for(let i=0;i<moves.length;i++) {
         let move=moves[i];
         let cell=id(move[0],move[1],n);
@@ -177,8 +197,11 @@ function randomBoard(n) {
     return res;
 }
 
-function updateScores(score_a, score_b) {
-    document.getElementById("score").innerText = score_a + " | " + score_b 
+function updateScores(score_a, score_b, turn=1) {
+    let scorecard=document.getElementById("score");
+    scorecard.classList.remove("p"+(turn*-1));
+    scorecard.classList.add("p"+(turn));
+    scorecard.innerText = score_a + " | " + score_b 
 }
 
 function bound(r, c, n) {
@@ -190,3 +213,82 @@ function bound(r, c, n) {
 
 start(generateBoard(8));
 // display(randomBoard(8));
+
+// find best move: minmax
+
+function copyBoard(board) {
+    let n=board.length;
+    let res=emptyBoard(n);
+    for(let i=0;i<n;i++) {
+        for(let j=0;j<n;j++) {
+            res[i][j]=board[i][j];
+        }
+    }
+    return res;
+}
+
+function minMax(board, turn, depth) {
+    let cb=copyBoard(board);
+    // turm 1: maximise, turn=-1: minimise
+    // console.log(depth);
+    let n=board.length;
+    let moves=validMoves(board, turn);
+    if(!depth||!moves.length)
+        return [evalStatic(board),-1,-1];
+    let eval=-inf; let bestMove=[-1,-1];
+    for(let i=0;i<moves.length;i++) {
+        let move=moves[i];
+        // if()
+        let changed=makeMove(board,move,turn);
+        for(let j=0;j<changed.length;j++) {
+            if(board[changed[j][0]][changed[j][1]]==0) {
+                throw new Error("bamboo");
+            }
+            board[changed[j][0]][changed[j][1]]=turn;
+        }
+        board[move[0]][move[1]]=turn;
+        let subtreeScore=minMax(board,turn*-1,depth-1)[0];
+
+        for(let j=0;j<changed.length;j++) {
+            board[changed[j][0]][changed[j][1]]=turn*-1;
+        }
+        board[move[0]][move[1]]=0
+
+        let bad=0;
+        for(let i=0;i<n;i++) {
+            for(let j=0;j<n;j++) {
+                bad|=cb[i][j]!=board[i][j];
+            }
+        }
+        if(bad) {
+            console.error("original board was ", cb);
+            console.error("somehow changed to", board);
+            console.error("move was ", move,"turn was ",turn);
+            console.error("changed was", changed);
+            throw new Error("lmao get rekt");
+        }
+        if(subtreeScore*turn>eval) {
+            eval=subtreeScore*turn;
+            bestMove=moves[i];
+        }
+    }
+
+    return [eval*turn,bestMove[0],bestMove[1]];
+}
+
+function evalStatic(board) {
+    // todo: give more weightage to corners and edges
+    let res=0,n=board.length;
+    for(let i=0;i<n;i++) {
+        for(let j=0;j<n;j++) {
+            let w=1;
+            if((i==0&&j==0)||(i==0&&j==n-1)||(i==n-1&&j==0)||(i==n-1&&j==n-1)) {
+                w=8;
+            } else if(i==0||i==n-1||j==0||j==n-1) {
+                w=4;
+            }
+            res+=board[i][j]*w;
+        }
+    }
+    return res;
+}
